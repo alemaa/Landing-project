19714a9d3c6bc2688244f44f722a9e140c236b1c
Indevitus <alema.duvnjak@edu.fit.ba>
2025-01-30 11:30:52 +0100

    edit:hero-buttons-update

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="container">
-<AppHeader/>
+<AppHeader
+@buy-button="onBuyButton"/>
 <main>
 <HeroSection title="Introduce Your Product Quickly & Effectively"
 description="Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
 Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus
 mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim."
-/>
+@primary-button="onPrimaryButton"
+@secondary-button="onSecondaryButton"/>
 <AboutSection/>
 <IntroSection/>
 <PricingSection/>
@@ -23,6 +25,15 @@ import HeroSection from './components/HeroSection.vue';
 import IntroSection from './components/IntroSection.vue';
 import PricingSection from './components/PricingSection.vue';
 
+const onBuyButton=() =>{
+    console.log('Received from child:', 'data');
+}
+const onPrimaryButton=() =>{
+    console.log('Received from child:', 'data');
+}
+const onSecondaryButton=() =>{
+    console.log('Received from child:', 'data');
+}
 </script>
 
 <style>
